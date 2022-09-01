204451a9655f6241c84e28ea40396a223bd59348
rasitHasanefendioglu <102167203+rasitHasanefendioglu@users.noreply.github.com>
2022-09-01 20:21:35 +0300

    Update MyWindow.cpp

--- OpenCV/simplePaint/MyWindow.cpp
+++ OpenCV/simplePaint/MyWindow.cpp
@@ -22,34 +22,3 @@ void MyWindow::createWindow(cv::Scalar color ) {
 	cv::imshow(winName, image);
 	cv::waitKey();
 }
-/*class MyWindow {
-protected:
-	cv::Size size;
-	cv::Mat image;
-public:
-	MyWindow();//default constructor
-	void setSize(int row,int col) {
-		size.height = row;
-		size.width = col;
-	}
-	MyWindow(int row, int col) {
-
-		size.height = row;
-		size.width = col;
-
-
-	}
-	virtual void createWindow(cv::Scalar color=cv::Scalar(255, 255, 255)) {
-		cv::Mat image(size, CV_8UC3, color);
-		cv::namedWindow(winName, 1);
-		if (image.empty())
-		{
-			std::cout << windowWarning;
-			return;    // Unsuccessful.
-		}
-		cv::imshow(winName, image);
-		cv::waitKey();
-	}
-
-
-};*/
