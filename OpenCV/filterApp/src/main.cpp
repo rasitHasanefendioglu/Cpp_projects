4e1cb22a96707afc90251d0b25cf87c2d3a93ecb
project <rasit.hasanefendioglu@gmail.com>
2025-04-14 14:39:54 +0300

    fix(filterApp): fix GUI for blurred filter

--- OpenCV/filterApp/src/main.cpp
+++ OpenCV/filterApp/src/main.cpp
@@ -1,21 +1,25 @@
 #include "app.hpp"
+#include "constants.h"
+
+void on_trackbar_change_gaussian_blur(int, void*);
 
 int main(int argc, char** argv) {
 
-    cv::Mat inputImage;
+    ImageDataGaussian data;
     
+    // Argument handling
     if(argc < 2) {
         std::cout << "Using example image, to use specific image please enter the path of the image.\n";;
-        inputImage = cv::imread("../share/exampleImage.jpeg");
-        if(inputImage.empty()) {
+        data.image = cv::imread("../share/exampleImage.jpeg");
+        if(data.image.empty()) {
             std::cerr << "Could not open or find the image!\n";
             std::cerr << "exampleImage.jpeg file can be corrupted!\n";
             return -1;
         }
     }
     else if(argc == 2) {
-        inputImage = cv::imread(argv[1]);
-        if(inputImage.empty()) {
+        data.image = cv::imread(argv[1]);
+        if(data.image.empty()) {
             std::cerr << "Could not open or find the image!\n";
             return -1;
         }
@@ -25,19 +29,51 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: ./filterApp <image_path>(optional)\n";
         return -1;
     }
-    int kernelSize = 5;
-    int sigma = 0;
-    bool colored = false;
-    /*cv::Mat blurredImage = blurImage(inputImage, kernelSize, sigma, colored);
-    if(blurredImage.empty()) {
-        std::cerr << "Blurring failed!\n";
-        return -1;
-    }*/
-    std::string windowName = "Original Image";
-    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
-    cv::imshow( windowName, inputImage);
-    //cv::imshow("Blurred Image", blurredImage);
-    cv::waitKey(0);
+
+    //Original image for reference
+    cv::namedWindow(window_original, cv::WINDOW_AUTOSIZE);
+    cv::imshow( window_original, data.image);
+
+    // Blurring part
+    cv::namedWindow(window_blurred, cv::WINDOW_AUTOSIZE);
+    cv::createTrackbar("Kernel Size", window_blurred, &data.kernelSize, 15, 
+        on_trackbar_change_gaussian_blur, &data);
+    cv::createTrackbar("Sigma", window_blurred, &data.sigma, 5, 
+        on_trackbar_change_gaussian_blur, &data);
+    cv::createTrackbar("Colored", window_blurred, &data.colored, 1, 
+        on_trackbar_change_gaussian_blur, &data);
+    on_trackbar_change_gaussian_blur(0, &data);
+
+
+    while(true) {
+        int key = cv::waitKey(100);  // Wait for 100ms for more responsive UI
+        if(key == 27) {  // ESC key
+            break;
+        }
+    }
+
+    // Clean up
     cv::destroyAllWindows();
     return 0;
+}
+
+void on_trackbar_change_gaussian_blur(int, void* userData) {
+
+    ImageDataGaussian* data = static_cast<ImageDataGaussian*> (userData);
+
+    //int kernelSize = cv::getTrackbarPos("Kernel Size", window_blurred);
+    //int sigma = cv::getTrackbarPos("Sigma", window_blurred);
+
+    if (data->kernelSize % 2 == 0) {
+        data->kernelSize += 1;
+    }
+    if (data->kernelSize < 3) {
+        data->kernelSize = 3;
+    }
+
+    // Apply Gaussian blur with the updated parameters
+    cv::Mat blurredImage = blurImageGaussian(*data);
+    
+    // Display the blurred image
+    cv::imshow(window_blurred, blurredImage);
 }
