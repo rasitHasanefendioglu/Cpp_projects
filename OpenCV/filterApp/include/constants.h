6e32a4045e6360fcbc376e82ba0a979b237ae5ee
project <rasit.hasanefendioglu@gmail.com>
2025-04-14 14:41:54 +0300

    refactor(filterApp): move constants to separate file

---
+++ OpenCV/filterApp/include/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+constexpr char window_original[] = "Original Image";
+constexpr char window_blurred[] = "Blurred Image";
+
+int gaussianKernelSize = 3;
+int gaussianSigma = 0;
+bool colored = false;
+#endif // CONSTANTS_H
+// This file contains constant definitions for the image processing application.
+// It is used in main.cpp only.
